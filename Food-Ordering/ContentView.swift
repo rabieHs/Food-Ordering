c5f91a284b4128b142f102a331edd88d31efb9e3
rabieHs <rabiehoussaini@gmail.com>
2024-06-26 22:57:38 +0100

    Initial Commit

---
+++ Food-Ordering/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food-Ordering
+//
+//  Created by rabie houssaini on 26/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food-Ordering/Food_OrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_OrderingApp.swift
+//  Food-Ordering
+//
+//  Created by rabie houssaini on 26/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Food_OrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
